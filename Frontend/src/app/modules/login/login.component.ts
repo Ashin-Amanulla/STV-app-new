e7623c174670a37aa5e6f453ca165b32a3e8cdd4
hello-world-ttj <tijotjoseph003@gmail.com>
2023-04-12 16:48:30 +0530

    Login page added...

--- Frontend/src/app/modules/login/login.component.ts
+++ Frontend/src/app/modules/login/login.component.ts
@@ -7,7 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  hideDiv: boolean = false;
+  showDiv: boolean = true;
+
   constructor(private router: Router) { }
+
+  getOtp() {
+    this.showDiv = !this.showDiv;
+    this.hideDiv = !this.hideDiv;
+  }
 
   login() {
     this.router.navigate(['/user']);
