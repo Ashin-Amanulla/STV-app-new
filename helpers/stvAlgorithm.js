8149c919599b2fb40c6274fc56db056c688ad8f8
Ashin Amanulla <ashin.jnv@gmail.com>
2023-04-27 12:50:58 +0530

    stv

--- helpers/stvAlgorithm.js
+++ helpers/stvAlgorithm.js
@@ -1,108 +1,92 @@
-function q() {
-    const getObjectKey = (obj, value) => Object.keys(obj).filter(key => obj[key] === value)
+//!-------------- MAIN ------ 
+const stvWinner = (candidateVotes) => {
+
+  let votes = candidateVotes;
+  const seats = 1; // Number of seats available
+
+  getWinner(votes, seats, (winner) => {
+    console.log(winner)
+  });
+
+
+
+}
+
+
+
+
+
+
+const getObjectKey = (obj, value) => Object.keys(obj).filter(key => obj[key] === value)
 
 //transfer vote of eliminated candidates
-const transferVotes = (cotes, candidateToEliminate) => votes.map(vote => vote.filter(x => x !== candidateToEliminate))
+const transferVotes = (votes, candidateToEliminate) => votes.map(vote => vote.filter(x => x !== candidateToEliminate))
 
-// Assuming you have an array of votes called `votes`
-const candidateVotes = [
-  ['Alice', 'Bob', 'Charlie'],
-  ['Alice', 'Bob', 'Charlie'],
-  ['Bob', 'Charlie', 'Alice'],
-  ['Charlie', 'Bob', 'Alice'],
-  ['Alice', 'Charlie', 'Bob'],
-  ['Bob', 'Charlie', 'Alice'],
-  ['Charlie', 'Bob', 'Alice'],
-  ['Charlie', 'Alice', 'Bob'],
-  ['Charlie', 'Alice', 'Bob'],
-  ['Alice', 'Charlie', 'Bob'],
-  ['Bob', 'Charlie', 'Alice'],
-  ['Bob', 'Alice', 'Charlie'],
-  ['Bob', 'Charlie', 'Alice'],
-  ['Charlie', 'Alice', 'Bob'],
-  ['Charlie', 'Bob', 'Alice'],
-  ['Alice', 'Charlie', 'Bob'],
-  ['Bob', 'Alice', 'Charlie'],
-  ['Charlie', 'Bob', 'Alice'],
-  ['Charlie', 'Alice', 'Bob'],
-  ['Alice', 'Bob', 'Charlie'],
-  [],
-  ['Alice'],
-  []
-];
+function getWinner(votes, seats, callback) {
+  let winner;
+  let voteCountArray = []; //to count the number of votes present for winner finding
 
-let votes = candidateVotes;
-const seats = 1; // Number of seats available
 
-getWinner(votes, (winner) => {
-  console.log(winner)
-});
+  const quota = Math.floor(totalVotes / (seats + 1)) + 1; // Calculate the quota
+  console.log(totalVotes)
 
-function getWinner(votes, callback) {
-  let winner
-  let voteCountArray = [];
-  
-  
-const quota = Math.floor(totalVotes / (seats + 1)) + 1; // Calculate the quota
-console.log(totalVotes)
-  
-  
-  let maxLength = votes.reduce((maxLength, currentValue) => currentValue && currentValue.length > maxLength ? currentValue.length : maxLength,0)
+// to find the longest array size so that ranking can be done
+  let maxLength = votes.reduce((maxLength, currentValue) => currentValue && currentValue.length > maxLength ? currentValue.length : maxLength, 0)
 
-  for (let i = 0; i < maxLength; i++) { //should change static given value '3'
+  for (let i = 0; i < maxLength; i++) { //to find the top rankings list of candidate or counting and so on
     let res = {};
     for (let j = 0; j < votes.length; j++) {
-      if (votes[j][i])	res[votes[j][i]] = (res[votes[j][i]] || 0) + 1
+      if (votes[j][i]) res[votes[j][i]] = (res[votes[j][i]] || 0) + 1
     }
     voteCountArray.push(res);
   }
   console.log(voteCountArray);
-  
-    let voteCount = voteCountArray[0];
-    for (const candidate in voteCount) {
-      let votesNo = voteCount[candidate];
-      if (votesNo >= quota) {
-        winner = candidate;
-        break;
-      }
+
+  let voteCount = voteCountArray[0];
+  for (const candidate in voteCount) {
+    let votesNo = voteCount[candidate];
+    if (votesNo >= quota) {
+      winner = candidate;
+      break;
     }
-  
+  }
+
   //if there is a tie
   console.log(Object.keys(voteCount))
   if (!winner && Object.keys(voteCount).length === 2) {
     winner = Object.keys(voteCount).toString()
-    gotWinner(winner)
-  } 
-  
-  if (winner) return gotWinner(winner)
-	
+    callback(winner)
+  }
+
+  if (winner) return callback(winner)
+
   let j = 0
   let candidateToEliminate
   while (j < voteCountArray.length && !candidateToEliminate) {
-   	let voteNos = voteCountArray[j]
+    let voteNos = voteCountArray[j]
     let lowest = totalVotes
     for (const candidate in voteNos) {
       if (lowest > voteNos[candidate]) {
         lowest = voteNos[candidate]
       }
     }
-    
+
     let candidate_with_lowest = getObjectKey(voteNos, lowest)
-   	//console.log("lowest -> ", lowest, candidate_with_lowest)
-    
+    //console.log("lowest -> ", lowest, candidate_with_lowest)
+
     //if length not 0, there are more than 1 candidates in last position
     if (candidate_with_lowest.length === 1) candidateToEliminate = candidate_with_lowest[0]
-    
+
     j++
   }
-  
+
   //console.log("candidateToEliminate ", candidateToEliminate)
-  
+
   //console.log(votes)
   votes = transferVotes(votes, candidateToEliminate)
   //console.log(votes)
-  
+
   getWinner(votes, callback)
 }
 
-}
+module.exports = stvWinner
